fbb75a5426165af2ad8a64a89c19c846c7f8b704
shivam-website <shivamsah1222@gmail.com>
2025-08-20 17:01:03 +0545

    Update app.py

--- app.py
+++ app.py
@@ -387,5 +387,7 @@ def create_shop():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+
+
 
